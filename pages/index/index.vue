b11a3f373cd04feb8307a1ee5a730ce5227276e9
Along0426 <2798902906@qq.com>
2022-12-01 15:34:08 +0800

    新功能:新建项目,引入公共样式表,字体图标库,动画样式,配置tabbar

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="index">
+		首页
+		<view class="content container">
+			首页
+		</view>
+		<view class="iconfont icon-search"></view>
+		<view class="animate__animated animate__zoomIn">1111</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/study/study.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="study">
+		学习
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
